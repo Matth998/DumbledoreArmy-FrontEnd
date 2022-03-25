3f52f5baa63f491f4799a61527bf73ec8964af0e
Matth998 <matheusoliveira.3125@gmail.com>
2022-03-24 23:13:58 -0300

    Menu and Footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
